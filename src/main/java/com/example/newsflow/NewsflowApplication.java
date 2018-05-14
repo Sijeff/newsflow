cbec9c96ffcfb990a8fbe515edf4c6677e3655c3
Simon Pellijeff <simonpellijeff@gmail.com>
2018-05-14 15:03:05 +0200

    Setup spring+react

---
+++ src/main/java/com/example/newsflow/NewsflowApplication.java
@@ -0,0 +1,12 @@
+package com.example.newsflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsflowApplication.class, args);
+	}
+}
