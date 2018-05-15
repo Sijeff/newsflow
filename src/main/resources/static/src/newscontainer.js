8ec24fe14fde3c5e48ccb2552f84f312fc837676
Simon Pellijeff <simonpellijeff@gmail.com>
2018-05-15 13:20:29 +0200

    Too much JS, static newsflow with dummy data

--- src/main/resources/static/src/newscontainer.js
+++ src/main/resources/static/src/newscontainer.js
@@ -1,8 +1,39 @@
 import React, { Component } from "react";
 
-class NewsContainer extends Component {
+class NewsItem extends Component {
     render() {
-        <NewsItem />
+        return (
+            <article className="newsItem">
+                <h3>{this.props.item.title}</h3>
+                <p>Av: <cite>{this.props.item.author}</cite></p>
+                <a href={this.props.item.link}>Gå till artikel</a>
+                <p><time>{this.props.item.pubDate}</time></p>
+            </article>
+        );
+    }
+}
+
+
+class NewsContainer extends Component {
+    // componentDidMount() {
+    //     fetch('/')
+    //     .then(results => {
+    //         return results.json();
+    //     }).then(function(myJson) {
+    //         console.log(myJson);
+    //     });
+
+    // }
+
+    render() {
+        return (
+            <div className="newsContainer">
+                <h2>{this.props.source.title}</h2>
+                <img className="image" src={this.props.source.image.url} alt="Källa"/>
+                <NewsItem item={this.props.source.item}/>
+            </div>
+            );
+
     }
 }
 
