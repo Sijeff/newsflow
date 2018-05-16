6f94816d790b0aec3634a7e0ba0c622947c122a4
Henrik Olsson <henrik.l.o@gmail.com>
2018-05-16 13:56:51 +0200

    Fix backend to frontend communication

--- src/main/java/com/example/newsflow/MainController.java
+++ src/main/java/com/example/newsflow/MainController.java
@@ -1,8 +1,7 @@
 package com.example.newsflow;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
@@ -10,7 +9,8 @@ public class MainController {
 
     private NewsFeed newsFeed;
 
-    @GetMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @CrossOrigin(origins = "*")
     public String showSomething() {
         return "HEJHEJHEJ";
     }
