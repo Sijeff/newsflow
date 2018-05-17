fae5d76a460dd88105f9051c405c6e3ffcb8da13
Simon Pellijeff <simonpellijeff@gmail.com>
2018-05-17 12:31:49 +0200

    Make newspaper feed print out all items

--- src/main/resources/static/src/index.js
+++ src/main/resources/static/src/index.js
@@ -6,45 +6,15 @@ import "./styles.css";
 
 
 let destination = document.querySelector("#root");
-
-const EXPRESSEN = {
-    channel: {
-        title: "Expressen: Nyheter",
-        link: "https://www.expressen.se/",
-        image: {
-            url: "https://www.expressen.se/Static/images/rss/getting_rss.png"
-        },
-        item: {
-            link: "https://www.expressen.se/nyheter/kungligt/efter-sveket-meghan-vadjar-att-pappan-ska-komma-pa-brollopet/",
-            author: "emelie.lundberg@expressen.se (Emelie Lundberg)",
-            title: "Efter sveket: Meghan vädjar att pappan ska komma på bröllopet",
-            pubDate: "Tue, 15 May 2018 10:32:47 +0200"
-        }
-    }
-};
-
-const AFTONBLADET = {
-    channel: {
-        title: "Aftonbladet: Sveriges nyhetskälla och mötesplats",
-        link: "http://www.aftonbladet.se/",
-        image: {
-            url: ""
-        },
-        item: {
-            link: "http://www.aftonbladet.se/sportbladet/hockey/a/3jdBxM/tv4-straffas-for-missen-under-shl-slutspelet",
-            author: "",
-            title: "TV4 straffas efter tittarnas ilska under SHL-slutspelet",
-            pubDate: "Tue, 15 May 2018 11:40:32 +0200"
-        }
-    }
-}
-
 class Content extends React.Component {
     constructor(props){
         super(props);
 
         this.state = {
-            items: []
+            newsPaper: {
+                title: '',
+                items: [{}]
+            }
         }
     }
     
@@ -52,13 +22,14 @@ class Content extends React.Component {
         axios.get("http://localhost:8080/")
       .then(res => {
         this.setState({
-            items: res.data.items
+            newsPaper: res.data
         })
       })
     }
     
 
     render() {
+        console.log(this.state.newsPaper);
         let contentStyle = {
             backgroundColor: "#FFDE00",
             fontFamily: "monospace",
@@ -67,7 +38,7 @@ class Content extends React.Component {
             <div className="main">
                 <h1 className="mainHeader">Nyhetsflöde</h1>
                 <div style={contentStyle} className="content">
-                    <NewsContainer items={this.state.items} />
+                    <NewsContainer newsPaper={this.state.newsPaper} />
                 </div>
             </div>
         );
--- src/main/resources/static/src/newscontainer.js
+++ src/main/resources/static/src/newscontainer.js
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 
 class NewsItem extends Component {
     render() {
-        console.log(this.props.item);
         return (
             <article className="newsItem">
-                <h3></h3>
-                <a href="">Gå till artikel</a>
-                <p><time></time></p>
+                <h3>{this.props.item.title}</h3>
+                <a href={this.props.item.link}>Gå till artikel</a>
+                <p><time>{this.props.item.date}</time></p>
             </article>
         );
     }
@@ -18,10 +17,12 @@ class NewsContainer extends Component {
     render() {
         return (
             <div className="newsContainer">
-                <h2>Title</h2>
-                <NewsItem item={this.props.items[0]}/>
+                <h2>{this.props.newsPaper.name}</h2>
+                {this.props.newsPaper.items.map(function (item, i) {
+                    return <NewsItem item={item} key={i} />;
+                })}
             </div>
-            );
+        );
 
     }
 }
